4eef5685c3f474cd4a3ff04d43406d888fccd32d
Andrey Kopitsa <andrey.kopitsa@gmail.com>
2024-03-25 09:15:33 +0000

    Update kbot

--- cmd/kbot.go
+++ cmd/kbot.go
@@ -45,6 +45,13 @@ to quickly create a Cobra application.`,
 		kbot.Handle(telebot.OnText, func(m telebot.Context) error {
 			//kbot.Send(m.Sender(), "Hello, "+m.Text)
 			log.Printf(m.Message().Payload, m.Text())
+			payload := m.Message().Payload
+
+			switch payload {
+			case "hello":
+				err = m.Send(fmt.Sprintf("Hello I'm kbot %s! ", appVersion))
+				
+			}
 			return err
 		})
 
